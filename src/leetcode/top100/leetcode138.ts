99e97f06b16f0c0a892afdc11cd12c0a47e78b62
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-03-27 19:03:41 -0400

    feat: adding

--- src/leetcode/top100/leetcode138.ts
+++ src/leetcode/top100/leetcode138.ts
@@ -3,12 +3,12 @@ import Node from "../../dataStructures/randomNode.ts";
 function copyRandomList(head: Node | null): Node | null {
   const map = new Map<Node | null, Node | null>();
   let runner = head;
-  
+
   while (runner) {
     map.set(runner, new Node(runner.val));
     runner = runner.next;
   }
-  
+
   runner = head;
 
   while (runner) {
@@ -19,10 +19,10 @@ function copyRandomList(head: Node | null): Node | null {
 
     node.next = map.get(runner?.next) ?? null;
     node.random = map.get(runner?.random) ?? null;
-    
+
     runner = runner.next;
   }
-  
+
   return map.get(head) ?? null;
 }
 
