0d3dd62065b84311e998780ec7cd318ad84acf24
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-04-06 17:39:11 -0400

    feat: adding more problems

---
+++ src/neetcode150/arraysAndHashing/leetcode128.ts
@@ -0,0 +1,5 @@
+function longestConsecutive(nums: number[]): number {
+  // this problem is honestly a graph one where we can utilize find and find connected components checking the length of each connected component
+}
+
+export default longestConsecutive;
