d43cb6a4e6c71e76ed004aec1f68da65881e55e9
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-04-21 19:08:08 -0400

    feat: updating with more problems

---
+++ src/neetcode150/linkedLists/leetcode141.ts
@@ -0,0 +1,19 @@
+import ListNode from "../../dataStructures/list.ts";
+
+function hasCycle(head: ListNode | null): boolean {
+  let slow = head;
+  let fast = head;
+  
+  while (slow && fast?.next && fast?.next?.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+export default hasCycle;
---
+++ src/neetcode150/linkedLists/leetcode2.ts
@@ -0,0 +1,35 @@
+import ListNode from "../../dataStructures/list.ts";
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const result = new ListNode(0);
+  let runner = result;
+
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null) {
+    let current = carry;
+
+    if (l1 !== null) {
+      current += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2 !== null) {
+      current += l2.val;
+      l2 = l2.next;
+    }
+
+    runner.next = new ListNode(current % 10);
+    runner = runner.next;
+
+    carry = Math.floor(current / 10);
+  }
+
+  if (carry > 0) {
+    runner.next = new ListNode(carry);
+  }
+
+  return result.next;
+}
+
+export default addTwoNumbers;
