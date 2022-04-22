22f5dff25689adb54c3bbac449d7fff7f5705b5c
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-04-22 17:11:59 -0400

    feat: updating stuff

--- src/neetcode150/linkedLists/leetcode287.ts
+++ src/neetcode150/linkedLists/leetcode287.ts
@@ -2,6 +2,7 @@
 function findDuplicate(nums: number[]): number {
   // this problem is not explicitly like a linked list problem
   // but we can adapt fast and slow pointers to solve this
+  // nums = [1,3,4,2,2]
 
   return -1;
 }
