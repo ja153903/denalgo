900e4e444cb46a58badbb9ec6504c90f9604f42b
Jaime Abbariao <jaimeabbariao@Jaimes-MacBook-Pro.local>
2022-04-23 10:21:50 -0400

    feat: updating with solution

--- src/neetcode150/linkedLists/leetcode287.ts
+++ src/neetcode150/linkedLists/leetcode287.ts
@@ -1,10 +1,26 @@
-// TODO: Solve this problem later
 function findDuplicate(nums: number[]): number {
-  // this problem is not explicitly like a linked list problem
-  // but we can adapt fast and slow pointers to solve this
-  // nums = [1,3,4,2,2]
+  if (nums.length < 1) {
+    return -1;
+  }
 
-  return -1;
+  let slow = nums[0];
+  let fast = nums[nums[0]];
+
+  // we do a slow and fast pointer approach here.
+  // and we break when fast and slow are the same
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  }
+
+  fast = 0;
+
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+
+  return slow;
 }
 
 export default findDuplicate;
